a04b2b26a33407f5301520f083a26dd8f3e63232
frodo <nosoysaenz@gmail.com>
2021-04-30 11:05:59 -0500

    knapsack bounded y unbounded

--- src/knapsack.c
+++ src/knapsack.c
@@ -5,14 +5,13 @@
 
 int **tasks;
 int **knapsack;
-int **changed;
 int **quantity;
 int capacity;
 int Q = 0;
 int parts = 0;
 
 int task_weight(int j, int k){
-    return tasks[j][2] * k;
+    return tasks[j][1] * k;
 }
 
 int prev(int i, int j){
@@ -20,56 +19,51 @@ int prev(int i, int j){
 }
 
 int get_max(int i, int j, int k){ 
-    int v_j = tasks[j][1] * k;
+    int v_j = tasks[j][0] * k;
     int c_j = task_weight(j, k);
     int new = v_j + knapsack[i-c_j][j-1];
     return prev(i, j) < new ? new : prev(i, j);
 }
 
+// bounded y 1 o 0 
 void bounded_knapsack(){
     for(int i = 0; i < capacity; i++){
         for(int j = 0; j < parts; j++){
             for(int k = 0; k <= Q; k++){
                 if(task_weight(j, k) <= i){
-                    knapsack[i][j] = get_max(i, j, k);
-                    if(knapsack[i][j] != prev(i, j)) changed[i][j] = 1;
-                    quantity[i][j] = k;
-                } else {
-                   // knapsack[i][j] = prev(i, j);
-                }  
+                    int t1 = get_max(i, j, k);
+                    int t2 = knapsack[i][j];
+                    if(t1 > t2){
+                        knapsack[i][j] = t1;
+                        quantity[i][j] = k;
+                    } 
+                } 
             }   
         }
     }
 }
 
-void unbounded_knapsack(){
+void unbounded_knapsack(){ 
     for(int i = 0; i < capacity; i++){
         for(int j = 0; j < parts; j++){
-
-        }
-    }
-}
-
-// knapsack 1 o 0
-void bin_knapsack(){
-    for(int i = 0; i < capacity; i++){
-        for(int j = 0; j < parts; j++){
-            if(task_weight(j, 1) <= i){
-                knapsack[i][j] = get_max(i, j, 1);
-                if(knapsack[i][j] != prev(i, j)) changed[i][j] = 1;
-                quantity[i][j] = 1;
-            } else {
-                knapsack[i][j] = prev(i, j);
-            }         
+            int k = 0;
+            while(task_weight(j, k) <= i){
+                int t1 = get_max(i, j, k);
+                int t2 = knapsack[i][j];
+                if(t1 > t2){
+                    knapsack[i][j] = t1;
+                    quantity[i][j] = k;
+                }
+                k++; 
+            }
         }
     }
 }
 
 void set_tasks(int p, int *values, int *weight){
-    for(int i = 0; i < p; i++){            
-        tasks[i][0] = i+1;
-        tasks[i][1] = values[i];
-        tasks[i][2] = weight[i];
+    for(int i = 0; i < p; i++){   
+        tasks[i][0] = values[i];
+        tasks[i][1] = weight[i];
     }
 }
 
@@ -77,9 +71,8 @@ void setup_knapsack(int max, int p, int c, int *v, int *w){
     Q = max;
     capacity = c + 1;
     parts = p;
-    tasks = init_matrix(p, 3);
+    tasks = init_matrix(p, 2);
     knapsack = init_matrix(capacity, p);
-    changed = init_matrix(capacity, p);
     quantity = init_matrix(capacity, p);
     set_tasks(p, v, w);
 }
@@ -88,29 +81,30 @@ int** get_solution(){
     int **sol = init_matrix(2, parts);
     int i = capacity-1;
     for(int j = parts-1; j >= 0; j--){
-        if(changed[i][j]){
+        if(quantity[i][j] != 0){
             sol[0][j] = j+1;
             sol[1][j] = quantity[i][j];
-            i = i - tasks[j][2];
-        }
+            i = i - task_weight(j, quantity[i][j]);
+        } 
     }
-    print_matrix(sol, 2, parts);
     return sol;
 }
 
-#define p 7
+#define p 3
+//#define p 7
 
 int main(int argc, char *argv[]){
     int v[p] = {11, 7, 12};
     int w[p] = {4, 3, 5};
-    int cap = 10;
-    //int v[p] = {7, 9, 5, 12, 14, 6, 12};
-    //int w[p] = {3, 4, 2, 6, 7, 3, 5};
-    //int cap = 15;
+    int cap = 10; 
+    /*int v[p] = {7, 9, 5, 12, 14, 6, 12};
+    int w[p] = {3, 4, 2, 6, 7, 3, 5};
+    int cap = 15; */
     // Q, # parts, capacity, values, weight
-    setup_knapsack(10, p, cap, v, w);
-    bounded_knapsack();
+    setup_knapsack(5, p, cap, v, w);
+    unbounded_knapsack();
     print_matrix(knapsack, capacity, p);
     printf("===== Sol =====\n");
-    get_solution();
+    int **sol = get_solution();
+    print_matrix(sol, 2, parts);
 }
