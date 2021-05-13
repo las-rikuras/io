92745d23f4853464ff788382d4a384c38367cd6d
Andres <nosoysaenz@gmail.com>
2021-05-13 13:02:44 -0600

    knapsack

--- src/knapsack_ui.c
+++ src/knapsack_ui.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include "knapsack.c"
 
 enum Algorithm {BOUNDED, UNBOUNDED, BINARY};
 GtkWidget *combo;
@@ -8,6 +9,7 @@ GtkWidget *spinnerMax;
 GtkWidget *tasks;
 GtkWidget *knapsack;
 GtkBuilder *builder;
+GtkWidget *window;
 
 guint32 task_number = 0;
 guint32 capacity_number = 0;
@@ -15,9 +17,6 @@ guint32 max_number = 0;
 enum Algorithm type = BINARY;
 
 int main(int argc, char *argv[]){
-
-    GtkWidget *window;
-
     gtk_init(&argc, &argv);
     builder = gtk_builder_new_from_file("glade/knapsack.glade");
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
@@ -39,25 +38,57 @@ void on_window_destroy(){
 }
 
 void on_load_clicked(){
-    printf("LOAD\n");
+    GtkWidget *dialog;
+    GtkFileChooser *chooser;
+    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
+    gint res;
+    GtkFileFilter *filter;
+
+    dialog = gtk_file_chooser_dialog_new("Open File", GTK_WINDOW(window), action, "Cancel", GTK_RESPONSE_CANCEL, "Open", GTK_RESPONSE_ACCEPT, NULL);
+    filter = gtk_file_filter_new();
+    gtk_file_filter_add_pattern(filter, "*.kn");
+    gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
+    chooser = GTK_FILE_CHOOSER(dialog);
+    res = gtk_dialog_run(GTK_DIALOG(dialog));
+    if(res == GTK_RESPONSE_ACCEPT){
+        char *fn;
+        fn = gtk_file_chooser_get_filename(chooser);
+        // load_knapsack
+        free(fn);
+    }
+    gtk_widget_destroy(dialog);  
 }
 
 void on_save_clicked(){
-    printf("SAVE\n");   
-}
+    GtkWidget *dialog;
+    GtkFileChooser *chooser;
+    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
+    gint res;
 
-void on_about_clicked(){
-    printf("ABOUT\n");   
+    dialog = gtk_file_chooser_dialog_new("Save File", GTK_WINDOW(window), action, "Cancel", GTK_RESPONSE_CANCEL, "Save", GTK_RESPONSE_ACCEPT, NULL);
+    chooser = GTK_FILE_CHOOSER(dialog);
+    gtk_file_chooser_set_current_name(chooser, "Untitled");
+    res = gtk_dialog_run(GTK_DIALOG(dialog));
+    if(res == GTK_RESPONSE_ACCEPT){
+        char *fn;
+        fn = gtk_file_chooser_get_filename(chooser);
+        // save_knapsack
+        free(fn);
+    }
+    gtk_widget_destroy(dialog);
 }
 
 void on_combo_changed(GtkEntry *e){
-    gchar txt = gtk_entry_get_text(e);
-    if(txt == "Bounded"){
+    const gchar *txt = gtk_entry_get_text(e);
+    if(strcmp(txt,"Bounded")){
         type = BOUNDED;
-    } else if (txt == "Unbounded") {
+        printf("1 ");
+    } else if (strcmp(txt,"Ubounded")) {
         type = UNBOUNDED;
+        printf("2 ");
     } else {
         type = BINARY;
+        printf("3 ");
     }
     printf("%s\n", txt);   
 }
